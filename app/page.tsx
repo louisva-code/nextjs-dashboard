f4949601d57385603a87e4d5f8d7fa1ae747b9f9
louisva-code <louis@genussoft.co.za>
2024-02-03 17:13:23 +0200

    Update Next.js Learn Course link in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course for dummies
+              Next.js Learn Course for dummies and all
             </a>
             , brought to you Vercel and me.
           </p>
